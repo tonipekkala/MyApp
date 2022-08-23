ecfa5aef2f7263dde79f576a91e260f22935a412
tonipekkala <toni.pekkala@metropolia.fi>
2022-08-24 01:54:55 +0300

    yes

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, FlatList, TouchableOpacity, Image,SafeAreaView } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
 
-export default function App() {
+const mediaArray = [
+  {
+    'key': '0',
+    'title': 'Title 1',
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
+    'thumbnails': {
+      w160: 'http://placekitten.com/160/161',
+    },
+    'filename': 'http://placekitten.com/2048/1920',
+  },
+  {
+    'key': '1',
+    'title': 'Title 2',
+    'description': 'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
+    'thumbnails': {
+      w160: 'http://placekitten.com/160/164',
+    },
+    'filename': 'http://placekitten.com/2041/1922',
+  },
+  {
+    'key': '2',
+    'title': 'Title 3',
+    'description': 'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
+    'thumbnails': {
+      w160: 'http://placekitten.com/160/167',
+    },
+    'filename': 'http://placekitten.com/2039/1920',
+  },
+];
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView  style={styles.container}>
+
+        <Text>Hei Open up App.js to start working on your app!</Text>
+
+        <FlatList
+          data={mediaArray}
+          renderItem={({item}) => {
+            return (
+              <TouchableOpacity style={styles.container2}>
+                <Image
+                  style={{width: 200, height: 200}}
+                  source={{uri: item.thumbnails.w160}}
+                />
+                <SafeAreaView style={styles.container3}>
+                  <Text style={{fontWeight: 'bold'}}>{item.title}</Text>
+                  <Text>{item.description}</Text>
+                </SafeAreaView >
+              </TouchableOpacity>
+            );
+          }}
+        />
+
+      </SafeAreaView >
+
+
+      <StatusBar style="auto"/>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e0e0e0',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container2: {
+    flex: 1,
+    backgroundColor: '#909090',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    width: 'auto',
+  },
+  container3: {
+    width: '50%',
+    marginLeft: '3%',
+  },
 });
+
+export default App;
