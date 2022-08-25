1f20ce57d7c1a71fa6b4259d0b6755f2714e9ff1
tonipekkala <toni.pekkala@metropolia.fi>
2022-08-25 23:52:03 +0300

    task b

--- components/ListItem.js
+++ components/ListItem.js
@@ -2,12 +2,14 @@ import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
 
 const ListItem = (props) => {
+  const mediaUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
+  // console.log(singleMedia);
   return (
     <TouchableOpacity style={styles.row}>
       <View style={styles.box}>
         <Image
           style={styles.image}
-          source={{uri: props.singleMedia.thumbnails.w160}}
+          source={{uri: mediaUrl + props.singleMedia.thumbnails.w160}}
         />
       </View>
       <View style={styles.box}>
